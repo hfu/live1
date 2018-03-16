83b5a18f6284fbe31ec0f3f410cbc3831fb349a3
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-03-16 22:19:33 +0000

    update

--- hook.js
+++ hook.js
@@ -4,8 +4,23 @@ MBTiles.registerProtocols(tilelive)
 
 tilelive.load('mbtiles://./test.mbtiles', (err, dst) => {
   if (err) console.log(err)
-  dst.startWriting(err => console.log)
-  console.log(dst.putTile(0, 0, 0, 1431534314, err => console.log))
-  dst.stopWriting(err => console.log)
+  dst.startWriting(err => { 
+    dst.putTile(0, 0, 0, 'adfadca', err => {
+      dst.stopWriting(err => console.log)
+    })
+  })
+  console.log('writing finished')
 })
 
+tilelive.load('mbtiles://./test.mbtiles', (err, src) => {
+  if (err) console.log(err)
+  console.log('getting info')
+  src.getInfo((err, info) => {
+    if (err) console.log(err)
+    console.log(info)
+  })
+//  src.getTile(0, 0, 0, (err, data, headers) => {
+//    if (err) console.log(err)
+//    console.log(data)
+//  })
+})
