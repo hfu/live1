2797c66f1e10491e69c24b8e79e2308bc6326b12
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-03-16 22:19:57 +0000

    update

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const tilelive = require('@mapbox/tilelive')
 require('tilelive-http')(tilelive)
 require('@mapbox/mbtiles').registerProtocols(tilelive)
 
+console.log(tilelive.list)
 
+/*
+const scheme = tilelive.Scheme.create('scanline', {
+  minzoom: 10, maxzoom: 11, bbox: [-118.9448, 32.8007, -117.6462, 34.8233]
+})
+
+let copyTask = new tilelive.CopyTask(
+  'http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
+  'mbtiles://./watercolor-la.mbtlies', scheme
+)
+copyTask.formats = ['tile']
+
+copyTask.start(err => {
+  if(err) throw err
+})
+
+copyTask.on('progress', console.log)
+copyTask.on('finished', () => {console.log('complete.')})
+*/
